dda13b8d4e5d0a6e28c7c36c11016030007e8909
Billyanz Yusuf <billyanz.yusufhs@gmail.com>
2026-09-09 17:08:29 +0700

    first commit

---
+++ js/config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: { sans: ['Inter', 'sans-serif'] },
+            colors: {
+                corporate: {
+                    900: '#0B1120',
+                    800: '#111827',
+                    700: '#1F2937',
+                    brand: '#2563EB',
+                    accent: '#10B981',
+                    light: '#F3F4F6'
+                }
+            }
+        }
+    }
+}
